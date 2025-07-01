ab74943f342c44d71e6ae5f2bf735b9767194c3f
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-01 13:53:13 +0700

    Add Home View

---
+++ SwiftfulThinkingSourceControlCourse/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulThinkingSourceControlCourse
+//
+//  Created by Dylan on 1/7/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
