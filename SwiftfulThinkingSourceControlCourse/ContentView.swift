4f47b0bfeee52e64e5d58ae8e636795bfa870721
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-02 00:22:44 +0700

    [Feature] Change icon in content view

--- SwiftfulThinkingSourceControlCourse/ContentView.swift
+++ SwiftfulThinkingSourceControlCourse/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "heart.fill")
+                Image(systemName: "swift")
                     .font(.largeTitle)
                     .foregroundStyle(color)
                     .onTapGesture {
