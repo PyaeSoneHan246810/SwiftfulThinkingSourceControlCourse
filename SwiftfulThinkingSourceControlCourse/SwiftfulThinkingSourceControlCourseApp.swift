b2d03423effc2f60b57797bd4ee64a0401744fd9
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-01 12:54:30 +0700

    Initial Commit

---
+++ SwiftfulThinkingSourceControlCourse/SwiftfulThinkingSourceControlCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSourceControlCourseApp.swift
+//  SwiftfulThinkingSourceControlCourse
+//
+//  Created by Dylan on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingSourceControlCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
