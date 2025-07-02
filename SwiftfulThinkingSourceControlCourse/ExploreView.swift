3e2f96aa90d5a9416c2ec9cd391d793f90791982
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-02 16:46:57 +0700

    [Feature] Add Explore View

---
+++ SwiftfulThinkingSourceControlCourse/ExploreView.swift
@@ -0,0 +1,23 @@
+//
+//  ExploreView.swift
+//  SwiftfulThinkingSourceControlCourse
+//
+//  Created by Dylan on 2/7/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        ScrollView {
+            
+        }
+        .navigationTitle("Explore")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ExploreView()
+    }
+}
