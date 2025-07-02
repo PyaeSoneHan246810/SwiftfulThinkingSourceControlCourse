bc6ddadc44375891ea98fabce070b3da87d60a98
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-07-02 21:38:07 +0700

    [Feature] Add Profile View

---
+++ SwiftfulThinkingSourceControlCourse/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulThinkingSourceControlCourse
+//
+//  Created by Dylan on 2/7/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
